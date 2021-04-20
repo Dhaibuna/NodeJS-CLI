a1cfc1edda71f5ca6be4893803d02a5c96447cba
Dhaibuna <nataschagritte@gmail.com>
2021-04-20 14:29:12 +0200

    Done step 4

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 #!/usr/bin/env node
 
+// Requires
+
+const chalk = require('chalk'); // Bonus : Chalk 
+const { getCode, getName} = require('country-list'); 
 
 // Ex 1 : Afficher ceci dans le terminal
 
-console.log("Hello, Node.JS!");
+console.log(chalk.cyan("Hello, Node.JS!"));
 
 // Ex 2 : Afficher "Belgique" apd du code "BE" 
-const { getCode, getName} = require('country-list');
 
-console.log(getCode('Belgium'));
+console.log(chalk.magenta.bold.bgWhiteBright(getCode('Belgium')));
 
 // Affiche le pays correspondant au code "CN"
 
-console.log(getName('CN'));
+console.log(chalk.yellow.bold.bgRed(getName('CN')));
+
+// Ex 3 : Http requests avec Axios ( install Axios)
+
+
+
 
 
 
